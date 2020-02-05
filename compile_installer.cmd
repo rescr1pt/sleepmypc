5ae3efc0e3b1b7051d2e47f6ba26a89918641d79
rescr1pt <rescr1pt@ya.ru>
2020-02-06 01:34:24 +0300

    add installer script

---
+++ compile_installer.cmd
@@ -0,0 +1,7 @@
+set SLEEPMYPC_SRC_EXE=msvc/x64/Release/sleepmypc.exe
+set SLEEPMYPC_ICO=src/sleepmypc.ico
+set SLEEPMYPC_COPYING=COPYING
+set SLEEPMYPC_README=README
+
+"C:\Program Files (x86)\Inno Setup 6\iscc.exe" /O"installer_out" installer.script
+pause
