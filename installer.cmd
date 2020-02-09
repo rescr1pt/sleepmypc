65de1a430ee3d0c138b1354a0d8bea7a6010ba37
rescr1pt <rescr1pt@ya.ru>
2020-02-10 02:10:05 +0300

    Added information to the README file and added the installer.

--- installer.cmd
+++ installer.cmd
@@ -3,5 +3,5 @@ set SLEEPMYPC_ICO=src/sleepmypc.ico
 set SLEEPMYPC_COPYING=COPYING
 set SLEEPMYPC_README=README
 
-"C:\Program Files (x86)\Inno Setup 6\iscc.exe" /O"installer_out" installer.script
+"C:\Program Files (x86)\Inno Setup 6\iscc.exe" /O"sleepmypc" installer.script
 pause
